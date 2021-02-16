5ce009d2c1f58a1bef8ea639017de0a3255653a2
Farnaz <farnaz.dp7394@gmail.com>
2021-02-16 12:46:10 +0330

    destructing test

--- src/App.js
+++ src/App.js
@@ -2,6 +2,27 @@
 import React from 'react'
 
 function App() {
+  const obj ={
+    name : "Asghar",
+    age : 25,
+    avg : 16.5,
+    Add : {
+      postcode : 5555,
+      address : "fkdfjdnf"
+    },
+    arr : [1,2,3,4,5]
+  }
+
+  const {name , age,Add} = obj
+  console.log(name)
+  console.log(age)
+  console.log(obj.name)
+  console.log(Add.address)
+
+  const {postcode} = obj.Add
+
+  const [a,_,b] = obj.arr
+  console.log(b)
   return (
     <div >
 
