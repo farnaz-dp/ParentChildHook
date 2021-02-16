fb0a08febe23cd533d233b145b0e88b308ebc057
Farnaz <farnaz.dp7394@gmail.com>
2021-02-16 16:18:50 +0330

    App Component is added

--- src/App.js
+++ src/App.js
@@ -1,32 +1,14 @@
 
 import React from 'react'
+import {Parent} from "./Components/Parent";
+
 
 function App() {
-  const obj ={
-    name : "Asghar",
-    age : 25,
-    avg : 16.5,
-    Add : {
-      postcode : 5555,
-      address : "fkdfjdnf"
-    },
-    arr : [1,2,3,4,5]
-  }
 
-  const {name , age,Add} = obj
-  console.log(name)
-  console.log(age)
-  console.log(obj.name)
-  console.log(Add.address)
-
-  const {postcode} = obj.Add
-
-  const [a,_,b] = obj.arr
-  console.log(b)
   return (
-    <div >
-
-    </div>
+      <div >
+        <Parent/>
+      </div>
   );
 }
 
